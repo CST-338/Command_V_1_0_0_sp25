04fa64b319a526d98981ef081a855020714724ed
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2025-02-19 11:18:30 -0800

    🎉 Started the template for the MCU inspired Command Assignment

---
+++ src/test/java/MCUTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class MCUTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+}
