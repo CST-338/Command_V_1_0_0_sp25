9deed9b72cd51b8dc0057a1909f7f3cf84758c33
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2025-03-03 12:28:12 -0800

    finished for monday

--- src/main/java/MCU.java
+++ src/main/java/MCU.java
@@ -1,3 +1,23 @@
 public class MCU {
 
+  /**
+   * TODO: Add toString to concrete implementations
+   * Todo: Make Concrete IStones Singletons.
+   * TODO: Make Deactivate in PowerStone
+   * TODO: Implement SpaceStone
+   * TODO: make a new implementation of person.
+   */
+  public static void main(String[] args) {
+    Titan thanos = new Titan("Thanos");
+
+    PowerStone powerStone = new PowerStone(thanos);
+
+    thanos.addStone(powerStone);
+
+    for (int i = 0; i < 4; i++) {
+      thanos.useGauntlet();
+    }
+
+  }
+
 }
