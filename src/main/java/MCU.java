07e3b1d36c4bca3f95d7d0a0fdafccf856d700c1
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2025-03-05 10:43:17 -0800

    implemented a SpaceStone

--- src/main/java/MCU.java
+++ src/main/java/MCU.java
@@ -5,8 +5,11 @@ public class MCU {
   public static void main(String[] args) {
     Titan thanos = new Titan("Thanos", "Earth");
     PowerStone powerStone = PowerStone.getPowerStone(thanos);
+    SpaceStone spaceStone = SpaceStone.getSpaceStone(thanos);
+    spaceStone.setNewLocation("Knowhere");
 
     thanos.addStone(powerStone);
+    thanos.addStone(spaceStone);
 
     for (int i = 0; i < 4; i++) {
       thanos.useGauntlet();
---
+++ src/main/java/SpaceStone.java
@@ -0,0 +1,55 @@
+/**
+ * @author Drew "Dr.C" Clinkenbeard
+ * <br>
+ * created: 3/5/25
+ * @since 0.1.0
+ */
+public class SpaceStone implements IStone{
+
+  private String oldLocation;
+  private String newLocation;
+  private Person owner;
+  private static SpaceStone spaceStone;
+
+  public static SpaceStone getSpaceStone(Person owner){
+    if(SpaceStone.spaceStone == null){
+      spaceStone = new SpaceStone(owner);
+    }
+    return spaceStone;
+  }
+
+  private SpaceStone(Person owner){
+    this.owner = owner;
+  }
+
+  public String getOldLocation() {
+    return oldLocation;
+  }
+
+  public void setOldLocation(String oldLocation) {
+    this.oldLocation = oldLocation;
+  }
+
+  public String getNewLocation() {
+    return newLocation;
+  }
+
+  public void setNewLocation(String newLocation) {
+    this.newLocation = newLocation;
+  }
+
+  @Override
+  public void activate() {
+    if(newLocation == null){
+      System.out.println("No location to move to.");
+      return;
+    }
+    if(owner.getLocation().equals(newLocation)){
+      System.out.printf("%s is already at %s%n",owner.getName(),newLocation);
+      return;
+    }
+    oldLocation = owner.getLocation();
+    System.out.printf("%s is moving from %s to %s%n",owner.getName(), oldLocation, newLocation);
+    owner.setLocation(newLocation);
+  }
+}
